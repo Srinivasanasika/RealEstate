60ba797eb3045e1f6cde5b52d9155f1541d37dd8
SrinivasaNasika <SrinivasaNasika@DESKTOP-R076OE3.in.ibm.com>
2019-11-13 17:02:27 +0530

    Basic Level TC RETC_20 and RETC_21

---
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTestsRealEsgtate_021.java
@@ -0,0 +1,68 @@
+package com.training.sanity.tests;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.training.generics.ScreenShot;
+import com.training.pom.LoginPOM;
+import com.training.pom.LoginPOMRealEstate;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class LoginTestsRealEsgtate_021 {
+
+	private WebDriver driver;
+	private String baseUrl;
+	private LoginPOMRealEstate loginPOM;
+	private static Properties properties;
+	private ScreenShot screenShot;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		properties = new Properties();
+		FileInputStream inStream = new FileInputStream("./resources/others.properties");
+		properties.load(inStream);
+	}
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		driver = DriverFactory.getDriver(DriverNames.CHROME);
+		loginPOM = new LoginPOMRealEstate(driver); 
+		baseUrl = properties.getProperty("baseURL");
+		screenShot = new ScreenShot(driver); 
+		// open the browser 
+		driver.get(baseUrl);
+	}
+	
+	@AfterMethod
+	public void tearDown() throws Exception {
+		Thread.sleep(1000);
+		//driver.quit();
+	}
+	@Test
+	public void validLoginTest() {
+		loginPOM.sendUserName("administrator");
+		loginPOM.sendPassword("administrator@123");
+		loginPOM.clickLoginBtn(); 
+		//loginPOM.addnewtag();
+		loginPOM.deltetag();
+		
+		screenShot.captureScreenShot("First");
+		
+		
+
+		
+	}
+}
